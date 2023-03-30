5a1bcc9d45645277b5470d57646e5afc347c88de
subhajitroy <subhajit.r@gmail.com>
2023-03-30 12:55:53 +0530

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 <body>
 
   <h1><b>P</b>rogramming Languages, Automated <b>R</>easoning, <b>A</b>rtificial <b>I</b>ntelligence and <b>S</b>oftware <b>E</b>ngineering</h1>
+  
+<?php
+echo "Hello World!";
+?>
 
 </body>
 </html> 
